010dd5f45798fc55cd6cba5f5795ecb6b99d459a
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-08-24 15:18:20 +0100

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { PlayerProvider } from "@/contexts/PlayerContext";
+import MusicPlayer from "@/components/MusicPlayer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlayerProvider>
+      <Component {...pageProps} />
+      <MusicPlayer />
+    </PlayerProvider>
+  );
 }
