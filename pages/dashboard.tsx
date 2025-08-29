88daa0647da8544e49937616b36899d51bc9ec82
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-08-29 12:01:50 +0100

    ..

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -138,8 +138,12 @@ const Home = () => {
   return (
     <Layout title="Home">
       <section className="mb-8">
-        <h2 className="text-xl font-bold mb-6 text-white">
-          Recommended for you
+        <h1>Welcome to Audio4lab</h1>
+        <h2 className="text-3xl font-bold mt-3 text-white">
+        Your creative journey starts now
+        </h2>
+        <h2 className="text-xl font-bold mb-6  text-white">
+        Recommend for you
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
           {homeContent.map((track) => (
