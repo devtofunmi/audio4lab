fe2ff94a40587e9bf1a881610cabb26f1823353c
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-08-30 02:09:07 +0100

    ..

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -155,7 +155,7 @@ const Home = () => {
         Its time to begin your creative journey.
         </h2>
         <h2 className="text-lg font-bold mb-6 mt-5 text-white">
-        Recommend for you
+        Explore music to match your project
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6">
           {homeContent.map((track) => (
