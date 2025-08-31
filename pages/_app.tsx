1aa1f94568fe93d0ad1b7155d431d63f4c2d76fc
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-08-31 01:37:31 +0100

    add geist custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PlayerProvider } from "@/contexts/PlayerContext";
 import MusicPlayer from "@/components/MusicPlayer";
+import { GeistSans } from "geist/font/sans";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PlayerProvider>
-      <Component {...pageProps} />
-      <MusicPlayer />
+      <main className={`${GeistSans.variable} font-sans`}>
+        <Component {...pageProps} />
+        <MusicPlayer />
+      </main>
     </PlayerProvider>
   );
 }
