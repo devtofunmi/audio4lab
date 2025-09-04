4d1560e9e093d295ca5d4990d5a35a324a9721c1
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-09-04 02:59:02 +0100

    ..

---
+++ prisma/migrations/20250904011003_add_plan_to_user/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "dateOfBirth" TIMESTAMP(3),
+    "language" TEXT,
+    "musicInterests" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "userRole" TEXT,
+    "plan" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
